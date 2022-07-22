66bc51422dbf4df5e190327b1358e4a4cff0cdde
dumont florian <sitix91@hotmail.fr>
2022-07-22 15:07:08 +0200

    initialisation du projet sur git

--- php/layout/metadata.php
+++ php/layout/metadata.php
@@ -4,5 +4,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/c72217183e.js" crossorigin="anonymous"></script>
     <title>Teyvat.fr</title>
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
