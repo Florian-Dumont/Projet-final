f5e9f583839470bb048717230837768a2b6f72b7
Florian-Dumont <floriandumont@ip-172-31-21-241.eu-west-3.compute.internal>
2022-07-26 14:07:47 +0000

    index cards

--- php/page/index.php
+++ php/page/index.php
@@ -4,8 +4,11 @@
     <body>
         <?php include "../layout/header.php" ?>
         <?php include "../layout/navigation.php" ?>
-        
-        <div><a class="twitter-timeline" data-lang="fr" data-width="400" data-height="400" href="https://twitter.com/genshinimpactfr?ref_src=twsrc%5Etfw">Tweets by genshinimpactfr</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+        <div class= sociaux-timeline>
+            
+        <div><a class="twitter-timeline" data-lang="fr" data-width="300" data-height="400" href="https://twitter.com/genshinimpactfr?ref_src=twsrc%5Etfw">Tweets by genshinimpactfr</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+        <a class="interactive-map" href="https://www.genshin-impact.fr/map/"><div class="interactive-map"><div class="map-sign">Map interactive</div></div></a>
+        </div>
         
         
     </body>
