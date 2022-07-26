2a19b5da57cf5ad7d8ab64b5d96a53fcfebce700
Florian-Dumont <floriandumont@ip-172-31-21-241.eu-west-3.compute.internal>
2022-07-26 10:25:04 +0000

    twitter feed

--- php/page/index.php
+++ php/page/index.php
@@ -4,5 +4,9 @@
     <body>
         <?php include "../layout/header.php" ?>
         <?php include "../layout/navigation.php" ?>
+        
+        <div><a class="twitter-timeline" data-lang="fr" data-width="400" data-height="400" href="https://twitter.com/genshinimpactfr?ref_src=twsrc%5Etfw">Tweets by genshinimpactfr</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+        
+        
     </body>
 </html>
