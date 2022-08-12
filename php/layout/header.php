0b3dcfc172948119cf8925d8f8db4be243cd9b18
Florian-Dumont <floriandumont@ip-172-31-24-128.eu-west-3.compute.internal>
2022-08-12 11:47:24 +0000

    logout init

--- php/layout/header.php
+++ php/layout/header.php
@@ -20,7 +20,19 @@
             
             ?>
         </a>
-        <a class="log-out" href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+        <a class="log-out" href="index.php"><i class="fa-solid fa-arrow-right-from-bracket">
+            
+            <?php
+ 
+// Déconnexion de l'utilisateur
+$_SESSION = array();
+session_unset();
+session_destroy();
+
+ 
+?>
+            
+        </i></a>
     </div>
     <div class="logo">
         <a href="index.php">
